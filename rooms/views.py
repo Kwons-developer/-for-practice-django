3ff25750ed0993c0c6b151357debc9dcedb15d39
Kwon <anstjdrnjs3@naver.com>
2021-04-10 15:45:04 +0900

    make templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from . import models
 
 
 def all_rooms(request):
-    return render(request, "all_rooms")
+    page = int(request.GET.get("page", 0))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
